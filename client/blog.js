390019df9c68b3a97b3f420e110f8c7f5d14c5cd
nirajmchauhan <nirajmchauhan@gmail.com>
2016-07-13 17:16:39 +0530

    Initial React Setup

---
+++ client/blog.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<p>Welcome to my blog!!</p>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './client/blog.js',
+  output: { path: __dirname, filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
